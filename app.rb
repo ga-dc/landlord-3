219453734f47bd3f2b7ef4ca73674072cc9201d9
Erica Irving <contact.eirving@gmail.com>
2015-07-21 17:34:47 -0400

    add links to redirect RENTER anchors to apartments/:id/:tenants

--- app.rb
+++ app.rb
@@ -10,27 +10,28 @@ get '/apartments' do
   if @renters == 0
     @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
-    @apartment_status = "RENTER lives in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/:id/:tenants'><a href='/apartments/:id/:tenants'>RENTER</a></a> lives in <a href='/apartments/:id'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "RENTER and RENTER live in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
   end
   erb :apartments
 end
 
 get '/apartments/:id' do
-  @renters = 2
+  @renters = 1
   if @renters == 0
     @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month. RENTER lives in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> lives in <a href='/apartments/:id'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month. RENTER and RENTER live in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
   end
   erb :apartments
 end
 
 get '/apartments/:id/:tenants' do
-  erb :apartments
+  "Howdy"
+  # erb :apartments
 end
 
 get '/apartments/new' do
