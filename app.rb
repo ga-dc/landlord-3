3888f139b4aeceb765674a474bfbaa69299a8ee3
Erica Irving <contact.eirving@gmail.com>
2015-07-21 17:11:58 -0400

    set up basic get methods and paths for required routes

--- app.rb
+++ app.rb
@@ -9,16 +9,18 @@ get '/apartments' do
   erb :apartments
 end
 
+get '/apartments/new' do
+  erb :add_apartment
+end
 
+get '/apartments/:id' do
+  erb :apartments
+end
 
-# - The route `GET /apartments/new` should show a form for adding a new apartment
-#   * Make sure to get the appropriate input from the user when creating an apartment
+get '/apartments/:id/:tenants' do
+  erb :apartments
+end
 
-# - The route `GET /apartments/1` should show info about a single apartment
-#   * Tell the user the address, monthly_rent, sqft, num_beds, num_baths, and renters
-
-# - The route `GET /apartments/1/tenants` should list all tenants for 1 apartment.
-
-# - The route `GET /apartments/1/tenants/new` should show a form for adding a new tenant.
-#   * Make sure to get the appropriate input from the user to create your person
-#   * __NOTE:__ Only two people can live in an apartment due to zoning laws (at least until you get to the bonus).
+get '/apartments/:id/:tenants/new' do
+  erb :add_tenant
+end
