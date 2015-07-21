773fff1cfa6646ee46b16e451de5e377af8bbb41
Erica Irving <contact.eirving@gmail.com>
2015-07-21 17:28:11 -0400

    add links to redirect ADDRESS anchors to apartments/:id

--- app.rb
+++ app.rb
@@ -8,18 +8,18 @@ end
 get '/apartments' do
   @renters = 2
   if @renters == 0
-    @apartment_status = "ADDRESS is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
-    @apartment_status = "RENTER lives in ADDRESS."
+    @apartment_status = "RENTER lives in <a href='/apartments/:id'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "RENTER and RENTER live in ADDRESS."
+    @apartment_status = "RENTER and RENTER live in <a href='/apartments/:id'>ADDRESS</a>."
   end
   erb :apartments
 end
 
-
 get '/apartments/:id' do
-  erb :apartments
+  "Howdy"
+  # erb :apartments
 end
 
 get '/apartments/:id/:tenants' do
