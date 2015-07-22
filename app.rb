387f41810045b8eedb45d1f504f908e73b7b0ec8
Melissa Steffan <msteffan2012@gmail.com>
2015-07-22 19:22:53 -0400

    added apartment.erb

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-    @menu = "Welcome to the landlord command center! What would you like to do today?"
+    @title = "Welcome to the landlord command center! What would you like to do today?"
     @apartments = "/apartments"
     @tenants = "/tenants"
     erb :index
 end
 
 get '/apartments' do
-
+    @title = "Here are the properties you manage:"
     erb :apartment
 end
