b22be087f83fdf6ce5672f2a953615cc4f093e43
Erica Irving <contact.eirving@gmail.com>
2015-07-21 20:55:10 -0400

    create forms for add apartment and add tenant

--- app.rb
+++ app.rb
@@ -8,35 +8,35 @@ end
 get '/apartments' do
   @renters = 0
   if @renters == 0
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month."
+    @apartment_status = "<a href='/apartments/1'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
-    @apartment_status = "<a href='/apartments/:id/:tenants'><a href='/apartments/:id/:tenants'>RENTER</a></a> lives in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/1/:tenants'><a href='/apartments/1/:tenants'>RENTER</a></a> lives in <a href='/apartments/1'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "<a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/1/:tenants'>RENTER</a> and <a href='/apartments/1/:tenants'>RENTER</a> live in <a href='/apartments/1'>ADDRESS</a>."
   end
   erb :apartments
 end
 
-get '/apartments/:id' do
+get '/apartments/1' do
   @renters = 0
   if @renters == 0
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month."
+    @apartment_status = "<a href='/apartments/1'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> lives in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/1'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/1/:tenants'>RENTER</a> lives in <a href='/apartments/1'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/1'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/1/:tenants'>RENTER</a> and <a href='/apartments/1/:tenants'>RENTER</a> live in <a href='/apartments/1'>ADDRESS</a>."
   end
   erb :apartments
 end
 
-get '/apartments/:id/:tenants' do
+get '/apartments/1/:tenants' do
   @renters = 2
   if @renters == 0
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is currently unoccupied."
+    @apartment_status = "<a href='/apartments/1'>ADDRESS</a> is currently unoccupied."
   elsif @renters == 1
-    @apartment_status = "<a href='/apartments/:id/:tenants'>RENTER</a> lives in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/1/:tenants'>RENTER</a> lives in <a href='/apartments/1'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "<a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/1/:tenants'>RENTER</a> and <a href='/apartments/1/:tenants'>RENTER</a> live in <a href='/apartments/1'>ADDRESS</a>."
   end
   erb :apartments
 end
@@ -46,11 +46,10 @@ get '/apartment_search' do
 end
 
 get '/apartments/new' do
-  # Collect a form input, then redirect to /apartments/:id
   erb :add_apartment
 end
 
-get '/apartments/:id/:tenants/new' do
-  # Collect a form input, then redirect to /apartments/:id/:tenants
+get '/apartments/1/:tenants/new' do
+  # Collect a form input, then redirect to /apartments/1/:tenants
   erb :add_tenant
 end
