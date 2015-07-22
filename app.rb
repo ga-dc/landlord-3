44aa77d8194d43c0b86a4801f037786e1ae00a28
Erica Irving <contact.eirving@gmail.com>
2015-07-21 20:38:53 -0400

    create path from view option on index to apartment search to apartment info page

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ get '/' do
 end
 
 get '/apartments' do
-  @renters = 2
+  @renters = 0
   if @renters == 0
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
     @apartment_status = "<a href='/apartments/:id/:tenants'><a href='/apartments/:id/:tenants'>RENTER</a></a> lives in <a href='/apartments/:id'>ADDRESS</a>."
   elsif @renters == 2
@@ -18,26 +18,39 @@ get '/apartments' do
 end
 
 get '/apartments/:id' do
-  @renters = 1
+  @renters = 0
   if @renters == 0
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month."
   elsif @renters == 1
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> lives in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> lives in <a href='/apartments/:id'>ADDRESS</a>."
   elsif @renters == 2
-    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM-BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is SQFT square feet and has NUM_BEDS beds and NUM_BATHS baths. It costs MONTHLY_RENT per month. <a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
   end
   erb :apartments
 end
 
 get '/apartments/:id/:tenants' do
-  "Howdy"
-  # erb :apartments
+  @renters = 2
+  if @renters == 0
+    @apartment_status = "<a href='/apartments/:id'>ADDRESS</a> is currently unoccupied."
+  elsif @renters == 1
+    @apartment_status = "<a href='/apartments/:id/:tenants'>RENTER</a> lives in <a href='/apartments/:id'>ADDRESS</a>."
+  elsif @renters == 2
+    @apartment_status = "<a href='/apartments/:id/:tenants'>RENTER</a> and <a href='/apartments/:id/:tenants'>RENTER</a> live in <a href='/apartments/:id'>ADDRESS</a>."
+  end
+  erb :apartments
+end
+
+get '/apartment_search' do
+  erb :apartment_search
 end
 
 get '/apartments/new' do
+  # Collect a form input, then redirect to /apartments/:id
   erb :add_apartment
 end
 
 get '/apartments/:id/:tenants/new' do
+  # Collect a form input, then redirect to /apartments/:id/:tenants
   erb :add_tenant
 end
